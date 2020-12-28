777b237add36489282640736f75da5c8ab64be14
dasyx <seanpnn00@gmail.com>
2020-12-28 15:40:23 +0100

    ajout label id sur option choix + commentaires

--- javascript/produit.js
+++ javascript/produit.js
@@ -11,7 +11,7 @@ function getAllBears(url){
     }
   })
 }
-//URL de l'API
+// URL de l'API
 const url = "http://localhost:3000/api/teddies";
 
 // Récupération des paramètres de l'URL
@@ -21,21 +21,23 @@ const idTeddy = params.get("id");
 getAllBears("http://localhost:3000/api/teddies/" + idTeddy)
 
 .then(function(response){
-// Fourni l'affichage selon les données du produit
+// Création de l'affichage selon les données du produit
 let choixProduit = document.getElementById("teddy_choice");
 
 let choixCont = document.createElement("section");
 let choixImage = document.createElement("div");
 let choixText = document.createElement("div");
-let choixOpt = document.createElement("div");
 let choixPhoto = document.createElement("img");
 let choixName = document.createElement("h3");
 let choixPrice = document.createElement("p");
+let choixOpt = document.createElement("div");
+let choixLabel = document.createElement("label");
+choixLabel.htmlFor ="choix_option";
 let choixColor = document.createElement("select");
 let choixAjout = document.createElement("div");
 let choixLink = document.createElement("a");
 
-//Ajout des attributs au balise index HTML 
+// Ajout des attributs à la page produit.html
 choixCont.setAttribute("class", "choix");
 choixImage.setAttribute("class", "choix_illustration");
 choixPhoto.setAttribute("src", response.imageUrl);
@@ -44,10 +46,11 @@ choixText.setAttribute("class", "choix_description");
 choixName.setAttribute("class", "choix_name");
 choixPrice.setAttribute("class", "choix_price");
 choixOpt.setAttribute("class", "choix_couleur");
+choixColor.setAttribute("id", "choix_option");
 choixAjout.setAttribute("class", "choix_ajout_panier");
 choixLink.setAttribute("href", "panier.html?id=" + response._id);
 
-// Agencement des éléments index HTML
+// Agencement des éléments à la page produit.html
 choixProduit.appendChild(choixCont);
 choixCont.appendChild(choixImage);
 choixImage.appendChild(choixPhoto);
@@ -55,19 +58,23 @@ choixCont.appendChild(choixText);
 choixText.appendChild(choixName);
 choixText.appendChild(choixPrice);
 choixCont.appendChild(choixOpt);
+choixOpt.appendChild(choixLabel);
 choixOpt.appendChild(choixColor);
 choixCont.appendChild(choixAjout);
 choixAjout.appendChild(choixLink);
 
-// Contenu des balises index HTML 
+// Contenu des balises en fonction de chaque article
 choixName.textContent = response.name;
 choixPrice.textContent = response.price / 100 + " euros";
+choixLabel.textContent = "Choisissez ma couleur :";
 choixLink.textContent = "Ajouter au panier";
 
+// Création d'une boucle for qui itère le résultat de la fonction qui récupère l'url de l'api  
   for (var i = 0; i < response.colors.length; i++) {
-    var option = document.createElement("option");
-    option.value = response.colors[i];
-    option.text = response.colors[i];
-    choixColor.appendChild(option);
-  }
+      // Création de chaque élément option en fonction du produit affiché
+      var option = document.createElement("option");
+      option.value = response.colors[i];
+      option.text = response.colors[i];
+      choixColor.appendChild(option);
+    }
 })
